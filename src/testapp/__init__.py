eb5d2176ac3cdc7a5d9d5ece4c444747356725ba
andrayantelo <andrayantelo@gmail.com>
2019-11-09 11:31:36 -0800

    put db file in instance folder

--- src/testapp/__init__.py
+++ src/testapp/__init__.py
@@ -9,11 +9,16 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, instance_relative_config=True)
     app.config['SECRET_KEY'] = 'DontTellAnyone'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///instance/db.sqlite3'
     
     db.init_app(app)
+    
+    @app.before_first_request
+    def create_tables():
+        from .models import Comment
+        db.create_all()
     
     from .views import main
     
